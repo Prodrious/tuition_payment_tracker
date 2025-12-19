a75855438787c68d919d4340f757a589882d4eeb
Prodrious <rohithprasad41@gmail.com>
2025-12-19 12:41:53 +0530

    API fix browser cache clear

--- server/server.js
+++ server/server.js
@@ -149,6 +149,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
+
+
 /* =====================
    DATABASE CONNECTION
 ===================== */
