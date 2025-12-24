08ebbcc4f372517e3c3712daab9f297f8f508e70
Prodrious <rohithprasad41@gmail.com>
2025-12-24 17:57:33 +0530

    fix for upfont payment2

--- server/server.js
+++ server/server.js
@@ -229,7 +229,7 @@ app.put('/api/students/:id/topup', async (req, res) => {
       {
         $inc: { balance: value },
         $push: { 
-          payments: { amount: value } 
+          payments: { amount: value, date: new Date.now } 
         }
       },
       { new: true }
